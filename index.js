30fc6551d458c6248845c3c7e6efe81fd883a30c
정경환 <sujkh@nomadconnection.com>
2018-06-26 13:59:51 +0900

    es5 test
    es5 test

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-export default (callback, timeout = 500) => {
-  let value = '';
-  let timer = null;
+module.exports = function(callback, timeout = 500) {
+  var value = '';
+  var timer = null;
 
-  let run = () => {
-    timer = setTimeout(() => {
+  var run = function() {
+    timer = setTimeout(function() {
       callback(value);
       clearTimeout(timer);
       timer = null;
     }, timeout);
   };
 
-  return e => {
+  return function(e) {
     value = e.target.value;
     if (!timer) {
       run();
